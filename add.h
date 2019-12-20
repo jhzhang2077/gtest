755908e4768406abf372cd5ecea21ded00d1904f
zjh <zjh95817@mail.ustc.edu.cn>
2019-12-20 19:04:28 +0800

    1

---
+++ add.h
@@ -0,0 +1,2 @@
+#pragma once  
+inline int Add(int i, int j) { return i+j; } 
---
+++ unittest.cpp
@@ -0,0 +1,18 @@
+#include "add.h"  
+#include "gtest\gtest.h"
+   
+TEST(Add, positive1) {  
+ EXPECT_EQ(Add(-1,-2), -3);  
+ EXPECT_GT(Add(-4,-5), -6);  
+}  
+   
+
+TEST(Add, positive2) {
+	EXPECT_EQ(Add(-1, -2), -3);
+	EXPECT_GT(Add(-4, -5), -6);
+}
+
+TEST(Add, negative) {  
+ EXPECT_EQ(Add(1,2), 3);  
+ EXPECT_GT(Add(4,5), 6);  
+} 
