0a1d7d658ae6ddc0274deedc89a70274d0eebb79
zjh <zjh95817@mail.ustc.edu.cn>
2019-12-25 11:33:45 +0800

    trigger

--- unittest.cpp
+++ unittest.cpp
@@ -1,6 +1,7 @@
 #include "add.h"  
 #include "gtest\gtest.h"
-   
+ 
+
 TEST(Add, positive1) {  
  EXPECT_EQ(Add(-1,-2), -3);  
  EXPECT_GT(Add(-4,-5), -6);  
@@ -16,3 +17,4 @@ TEST(Add, negative) {
  EXPECT_EQ(Add(1,2), 3);  
  EXPECT_GT(Add(4,5), 6);  
 } 
+
